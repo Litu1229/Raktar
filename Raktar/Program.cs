f641ddb0d5565dcb3378d913db1f4302ece905af
Ripperoni <lcsabi9d@gmail.com>
2020-09-23 14:55:24 +0200

    orai munka

--- Raktar/Program.cs
+++ Raktar/Program.cs
@@ -9,8 +9,21 @@ namespace Raktar
 {
     class Program
     {
+
+        static void beolvasraktar()
+        {
+            StreamReader raktar = new StreamReader("raktar.csv");
+            while (!raktar.EndOfStream)
+            {
+                string[] sor = raktar.ReadLine().Split(';');
+            }
+            raktar.Close();
+        }
         static void Main(string[] args)
         {
+            //termek t = new termek("P01", "Póló XXL", 1500, 10);
+            //Console.WriteLine($"{t.Kod}- {t.Nev}- {t.Ar}- {t.DB}";
+            Console.ReadKey();
         }
     }
 }
--- Raktar/termek.cs
+++ Raktar/termek.cs
@@ -9,5 +9,44 @@ namespace Raktar
 {
     class termek
     {
+        class Program
+        {
+            private string kod;
+
+
+            public string Kod
+            {
+                get { return kod; }
+                set { kod = value; }
+            }
+            private string nev;
+            public string Nev
+            {
+                get { return nev; }
+                set { nev = value; }
+            }
+            private int ar;
+
+            public int Ar
+            {
+                get { return ar; }
+                set { ar = value; }
+            }
+            private int db;
+            public int DB
+            {
+                get { return db; }
+                set { db = value; }
+            }
+
+
+            public termek(string kod, string nev, int ar, int db)
+            {
+                this.kod = kod;
+                this.nev = nev;
+                this.ar = ar;
+                this.db = db;
+            }
+        }
     }
 }
