699f6f27132482d3d6b03306f1291f8cfad06966
Ripperoni <lcsabi9d@gmail.com>
2020-09-25 09:04:03 +0200

    megrendeles beolvasasa

--- Raktar/Program.cs
+++ Raktar/Program.cs
@@ -10,6 +10,7 @@ namespace Raktar
     class Program
     {
         static List<termek> termekek = new List<termek>();
+        static List<megrendeles> megrendel = new List<megrendeles>();
         static void beolvasraktar()
         {
             StreamReader raktar = new StreamReader("raktar.csv");
@@ -20,14 +21,34 @@ namespace Raktar
             }
             raktar.Close();
         }
+
+        static void beolvasrendeles()
+        {
+            StreamReader rendeles = new StreamReader("rendeles.csv");
+            while (!rendeles.EndOfStream)
+            {
+                string[] sor2 = rendeles.ReadLine().Split(';');
+                //megrendel.Add(new megrendeles(sor2[1], sor2[2], sor2[3]));
+                if (sor2[0] == "M")
+                {
+                    megrendel.Add(new megrendeles(sor2[1], sor2[2], sor2[3]));
+                }
+            }
+            rendeles.Close();
+        }
         static void Main(string[] args)
         {
             //termek t = new termek("P01", "Póló XXL", 1500, 10);
             //Console.WriteLine($"{t.Kod}- {t.Nev}- {t.Ar}- {t.DB}");
              beolvasraktar();
-            foreach (var t in termekek)
+            beolvasrendeles();
+            /*foreach (var t in termekek)
             {
                 Console.WriteLine(t.Nev);
+            }*/
+            foreach (var m in megrendel)
+            {
+                Console.WriteLine($"{m.Datum} - {m.Email}- {m.Kod}");
             }
             Console.ReadKey();
         }
---
+++ Raktar/megrendeles.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Schema;
+
+namespace Raktar
+{
+    class megrendeles
+    {
+        private string datum;
+
+        public string Datum
+        {
+            get { return datum; }
+            set { datum = value; }
+        }
+
+        private string kod;
+
+        public string Kod
+        {
+            get { return kod; }
+            set { kod = value; }
+        }
+
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public megrendeles(string datum, string kod, string email)
+        {
+            this.datum = datum;
+            this.kod = kod;
+            this.email = email;
+        }
+
+    }
+}
